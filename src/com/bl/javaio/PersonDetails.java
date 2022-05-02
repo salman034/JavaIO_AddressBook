13f48aced3cb018a3c5039173f1e6eb08269b152
unknown <you@example.com>
2022-05-02 07:27:41 +0530

    Added: Ability to Search Persons details in the AddressBook

--- src/com/bl/javaio/PersonDetails.java
+++ src/com/bl/javaio/PersonDetails.java
@@ -10,8 +10,7 @@ public class PersonDetails {
     private String phoneNumber;
     private String email;
 
-    public PersonDetails(String firstName, String lastName, String address, String city, String state,
-                         String zipCode, String phoneNumber,  String email) {
+    public PersonDetails(String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -30,16 +29,8 @@ public class PersonDetails {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
     }
 
     public void setAddress(String address) {
@@ -62,24 +53,12 @@ public class PersonDetails {
         this.state = state;
     }
 
-    public String getZipCode() {
-        return zipCode;
-    }
-
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
